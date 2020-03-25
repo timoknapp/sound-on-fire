47cde855f43b14dd8db2da905ff0d100b0ede5c9
tea-mo903 <timo_knapp@hotmail.com>
2020-03-25 19:33:14 +0100

    [CICD] add release process while tagging

--- buildAndroid.sh
+++ buildAndroid.sh
@@ -3,15 +3,17 @@ appVersionName=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '
 appVersionCode=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f2)
 archiveName="SoundOnFire-v$appVersionName+$appVersionCode"
 fileType=".apk"
-path="build/app/outputs/apk"
+path="build/app/outputs/apk/$1"
 
 echo "Build .apk as artifact-type: $1 | $archiveName"
 
 # Build .apk with flutter command
-path="$path/$1"
+flutter pub get
 flutter build apk "--$1"
 fileName=$path/$archiveName-$1$fileType
+
 # Rename artifact
 mv -f "$path/app-$1.apk" "$fileName"
 echo "Moved: $fileName"
 echo $fileName > tmp_artifact_path.txt
+echo $archiveName$fileType > tmp_artifact_name.txt
