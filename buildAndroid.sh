bcc8802305d946ba42b0d109772cde48dce997af
tea-mo903 <timo_knapp@hotmail.com>
2020-03-25 20:02:52 +0100

    [CICD] small refactor

--- buildAndroid.sh
+++ buildAndroid.sh
@@ -1,8 +1,8 @@
 # Extract Version from pubspec.yaml
 appVersionName=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f1)
 appVersionCode=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f2)
-archiveName="SoundOnFire-v$appVersionName+$appVersionCode"
 fileType=".apk"
+archiveName="SoundOnFire-v$appVersionName+$appVersionCode-$1$fileType"
 path="build/app/outputs/apk/$1"
 
 echo "Build .apk as artifact-type: $1 | $archiveName"
@@ -10,10 +10,10 @@ echo "Build .apk as artifact-type: $1 | $archiveName"
 # Build .apk with flutter command
 flutter pub get
 flutter build apk "--$1"
-fileName=$path/$archiveName-$1$fileType
+fileName=$path/$archiveName
 
 # Rename artifact
 mv -f "$path/app-$1.apk" "$fileName"
 echo "Moved: $fileName"
 echo $fileName > tmp_artifact_path.txt
-echo $archiveName$fileType > tmp_artifact_name.txt
+echo $archiveName > tmp_artifact_name.txt
