c56b4a3ad944a1a9212b0d1b9da6a142fee58b8a
tea-mo903 <timo_knapp@hotmail.com>
2020-04-01 20:34:32 +0200

    [UPDATE] fix bug in build script

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sound_on_fire/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('SoundOnFire Tests', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
