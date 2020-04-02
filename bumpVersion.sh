01cc56ac95b1ec953155e6b20f8a02e1b3eb3af8
tea-mo903 <timo_knapp@hotmail.com>
2020-04-02 23:27:32 +0200

    [UPDATE] add bump version script

---
+++ bumpVersion.sh
@@ -0,0 +1,15 @@
+# Extract Version from pubspec.yaml
+appVersionName=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f1)
+appVersionCode=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f2)
+
+# Bump Version
+newAppVersionName=$appVersionName
+newAppVersionCode=$(expr $appVersionCode + 1)
+echo "Bump Version from $appVersionName+$appVersionCode to $newAppVersionName+$newAppVersionCode"
+
+old="version: $appVersionName+$appVersionCode"
+new="version: $newAppVersionName+$newAppVersionCode"
+echo "$old > $new"
+
+# Replace Version in pubspec.yaml
+sed -i '' "s/$old/$new/g" pubspec.yaml
