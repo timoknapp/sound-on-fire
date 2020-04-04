765fca64e5159d2457ced2f501953250610c74c8
Timo Knapp <timo_knapp@hotmail.com>
2020-04-04 19:45:28 +0200

    [UPDATE] add version number to loading screen

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:package_info/package_info.dart';
 import 'package:sound_on_fire/screens/home.dart';
 import 'package:sound_on_fire/util/constants.dart';
 import 'package:sound_on_fire/services/soundcloud.dart' as soundcloudService;
@@ -12,11 +13,25 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
 
   String clientId;
+  PackageInfo _packageInfo = PackageInfo(
+    appName: '',
+    packageName: '',
+    version: '',
+    buildNumber: '',
+  );
 
   @override
   void initState() {
+    _getPackageInfo();
     _getClientId();
     super.initState();
+  }
+
+  void _getPackageInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = packageInfo;
+    });
   }
 
   void _getClientId() async {
@@ -36,9 +51,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: SpinKitDoubleBounce(
-            color: primaryOrange,
-            size: 100.0,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                  child: SpinKitDoubleBounce(
+                  color: primaryOrange,
+                  size: 100.0,
+                ),
+              ),
+              Text("v${_packageInfo.version}+${_packageInfo.buildNumber}"),
+            ],
           ),
         ));
   }
