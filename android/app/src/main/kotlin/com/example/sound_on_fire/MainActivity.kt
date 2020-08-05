0ad96dd400f752f75f9b00e115c516ec96b1226b
timoknapp <mail@timoknapp.com>
2020-08-05 09:23:16 +0200

    [UPDATE] renamed android package name

--- android/app/src/main/kotlin/com/example/sound_on_fire/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sound_on_fire/MainActivity.kt
@@ -22,8 +22,6 @@ class MainActivity: FlutterActivity() {
                 .setUpdateFrom(UpdateFrom.JSON)
 //                .setUpdateJSON("https://raw.githubusercontent.com/timoknapp/sound-on-fire/master/app-update-changelog.json")
                 .setUpdateJSON("https://timoknapp.com/sound-on-fire/app-update-changelog.json")
-//                .setUpdateFrom(UpdateFrom.GITHUB)
-//                .setGitHubUserAndRepo("timoknapp", "sound-on-fire")
                 .start()
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
