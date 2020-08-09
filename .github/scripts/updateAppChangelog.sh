1e37a88b92065534872896907457d7460aa6c5bd
timoknapp <mail@timoknapp.com>
2020-08-09 22:36:55 +0200

    [UPDATE] fixed updateAppChangelog script

--- .github/scripts/updateAppChangelog.sh
+++ .github/scripts/updateAppChangelog.sh
@@ -1,24 +1,26 @@
 appVersionName=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f1)
 appVersionCode=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f2)
 
-oldVersionName=$(cat app-update-changelog.json | grep \"latestVersion\": | cut -d ':' -f2)
-oldVersionCode=$(cat app-update-changelog.json | grep \"latestVersionCode\": | cut -d ':' -f2)
-oldURL=$(cat app-update-changelog.json | grep \"url\":)
+oldVersionName=$(cat app-update-changelog.json | grep \"latestVersion\": | cut -d ':' -f2 | sed 's/\,//g' | sed 's/\"//g' | sed 's/\ //g' )
+oldVersionCode=$(cat app-update-changelog.json | grep \"latestVersionCode\": | cut -d ':' -f2 | sed 's/\,//g' | sed 's/\ //g' )
+# oldURL=$(cat app-update-changelog.json | grep \"url\":)
+# oldVersionEncoded="$oldVersionName%2B$oldVersionCode"
 
-newVersionName=" \"v$appVersionName\","
-newVersionCode=" $appVersionCode,"
-newURL="    \"url\": \"$1\","
+newVersionName="v$appVersionName"
+newVersionCode="$appVersionCode"
+# newVersionEncoded="v$appVersionName%2B$appVersionCode"
+# newURL=$($oldURL | sed 's#$oldVersionEncoded')
+
 echo "Update Changelog:"
 echo "$oldVersionName >> $newVersionName"
 echo "$oldVersionCode >> $newVersionCode"
-echo "$oldURL >> $newURL"
 
 # Replace Version in pubspec.yaml
 sed -i "s/$oldVersionName/$newVersionName/g" app-update-changelog.json
 sed -i "s/$oldVersionCode/$newVersionCode/g" app-update-changelog.json
-sed -i "s#$oldURL#$newURL#g" app-update-changelog.json
+## sed -i "s#$oldURL#$newURL#g" app-update-changelog.json
 ## --> MacOS
-#sed -i '' "s/$oldVersionName/$newVersionName/g" app-update-changelog.json
-#sed -i '' "s/$oldVersionCode/$newVersionCode/g" app-update-changelog.json
-#sed -i '' "s#$oldURL#$newURL#g" app-update-changelog.json
+# sed -i '' "s/$oldVersionName/$newVersionName/g" app-update-changelog.json
+# sed -i '' "s/$oldVersionCode/$newVersionCode/g" app-update-changelog.json
+## sed -i '' "s#$oldURL#$newURL#g" app-update-changelog.json
 git add app-update-changelog.json
