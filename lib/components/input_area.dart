e48dd2c6a358cb8980c8d214b4e8aa66df858070
timoknapp <mail@timoknapp.com>
2020-10-18 18:54:45 +0200

    chore(refactor): restructuring of some components in the home screen

---
+++ lib/components/input_area.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sound_on_fire/components/autocomplete_item.dart';
+import 'package:sound_on_fire/components/keyboard.dart';
+
+class InputArea extends StatelessWidget {
+  InputArea({
+    @required this.autocompleteItems,
+    @required this.onKeyboardAction,
+  });
+
+  final List<AutocompleteItem> autocompleteItems;
+  final Function onKeyboardAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 4,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Keyboard(
+              onKeyboardAction: onKeyboardAction,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: autocompleteItems,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/result_area.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sound_on_fire/components/track_tile.dart';
+
+class ResultArea extends StatelessWidget {
+  ResultArea({
+    @required ScrollController scrollController,
+    @required this.trackTiles,
+  }) : _scrollController = scrollController;
+
+  final ScrollController _scrollController;
+  final List<TrackTile> trackTiles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 5,
+      child: ListView(
+        controller: _scrollController,
+        scrollDirection: Axis.horizontal,
+        children: trackTiles,
+      ),
+    );
+  }
+}
