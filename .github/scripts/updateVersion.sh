04b70eb52b9b04aac075542564c0f497d7f24508
timoknapp <mail@timoknapp.com>
2020-11-08 10:21:30 +0100

    chore: enable semantic release first time

---
+++ .github/scripts/updateVersion.sh
@@ -0,0 +1,18 @@
+# Extract Version from pubspec.yaml
+appVersionName=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f1)
+appVersionCode=$(cat pubspec.yaml | grep "version: " | cut -d ' ' -f2 | cut -d '+' -f2)
+
+# Bump Version
+newAppVersionName=$1
+newAppVersionCode=$(expr $appVersionCode + 1)
+echo "Bump Version: $appVersionName+$appVersionCode >> $newAppVersionName+$newAppVersionCode"
+
+old="version: $appVersionName+$appVersionCode"
+new="version: $newAppVersionName+$newAppVersionCode"
+#echo "$old > $new"
+
+# Replace Version in pubspec.yaml
+sed -i "s/$old/$new/g" pubspec.yaml
+# --> MacOS
+# sed -i '' "s/$old/$new/g" pubspec.yaml
+git add pubspec.yaml
